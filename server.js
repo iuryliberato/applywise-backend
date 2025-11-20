7e9049d366c840b821d7ecbebfbfab048e144291
iuryliberato <iuryliberato@hotmail.com>
2025-11-20 14:57:06 +0100

    built auth

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+
+const testJwtRouter = require('./controllers/test-jwt');
+const authRouter = require('./controllers/auth');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// Routes go here
+app.get('/', (req, res) => {
+  res.json({ ok: true, path: req.path });
+});
+
+app.use('/test-jwt', testJwtRouter);
+app.use('/auth', authRouter);
+
+app.listen(3000, () => {
+  console.log("connected to port 3000")
+} )
+
+module.exports = app;
