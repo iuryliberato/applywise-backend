730ba8144045dda2f151fcb56dde627ea7e78107
iuryliberato <iuryliberato@hotmail.com>
2025-12-03 09:55:59 +0100

    deployment

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
 app.use('/job-applications', jobApplicationsRouter);
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
