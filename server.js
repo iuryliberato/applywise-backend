265b33d3eeafefb459df225147bd89277cb30198
iuryliberato <iuryliberato@hotmail.com>
2025-12-05 16:00:36 +0100

    fixing bug when trying to create a new application

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(logger('dev'));
 
+app.get('/health', (req, res) => {
+  res.json({ ok: true, message: 'Backend is running' });
+});
+
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
